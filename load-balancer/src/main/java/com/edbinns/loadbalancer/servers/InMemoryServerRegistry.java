42a4169c08ba24e4ffb9863f3e30fe4e0f03e3d0
Eduardo Binns <eduardo@honor.education>
2026-07-04 21:43:57 -0600

    Set up servers logic

---
+++ load-balancer/src/main/java/com/edbinns/loadbalancer/servers/InMemoryServerRegistry.java
@@ -0,0 +1,22 @@
+package com.edbinns.loadbalancer.servers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.edbinns.loadbalancer.config.LoadBalancerProperties;
+import com.edbinns.loadbalancer.models.ServerInstance;
+
+@Service
+public class InMemoryServerRegistry implements ServerRegistry {
+    private final LoadBalancerProperties properties;
+
+    public InMemoryServerRegistry(LoadBalancerProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public List<ServerInstance> getServers() {
+        return properties.getServers();
+    }
+}
